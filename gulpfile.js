14b4b6a33d7c890acae072d967d2c714e5a3eca7
Thomas Green <friar.festus@gmail.com>
2018-07-06 15:33:43 -0700

    Added CoffeeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
+var coffeeSources = ['components/coffee/tagline.coffee'];
 
-gulp.task('log', function(done) {
-    gutil.log('Workflows are awesome');
+gulp.task('coffee', function(done) {
+    gulp.src(coffeeSources)
+        .pipe(coffee({ bare: true })
+            .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'))
     done();
 });
